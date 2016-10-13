8856d99254e83f08f9ae91e8da15a892ff6d8c2b
starsriver <starsriver@outlook.com>
2016-10-13 16:17:47 +0800

    添加项目3 包含自动建议

---
+++ 3/MainPage.xaml.cs
@@ -0,0 +1,70 @@
+﻿using _3.Model;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.Activation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+//“空白页”项模板在 http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409 上有介绍
+
+namespace _3
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            rootFrame = Window.Current.Content as Frame;
+        }
+        private Frame rootFrame { get; set; }
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            // 设置显示导航栏后退按钮
+            if (rootFrame.CanGoBack)
+            {
+                SystemNavigationManager.GetForCurrentView().AppViewBackButtonVisibility = AppViewBackButtonVisibility.Visible;
+            }
+            else
+            {
+                SystemNavigationManager.GetForCurrentView().AppViewBackButtonVisibility = AppViewBackButtonVisibility.Collapsed;
+            }
+        }
+
+        private void AutoSuggestBox_TextChanged(AutoSuggestBox sender, AutoSuggestBoxTextChangedEventArgs args)
+        {
+            if(args.Reason==AutoSuggestionBoxTextChangeReason.UserInput)
+            {
+                IEnumerable<Contact> SuggestionContact = SuggestionDataSource.GetSuggestionContact(sender.Text);
+                sender.ItemsSource = SuggestionContact.ToList();
+           }
+        }
+
+        private void AutoSuggestBox_QuerySubmitted(AutoSuggestBox sender, AutoSuggestBoxQuerySubmittedEventArgs args)
+        {
+        }
+
+        private void AutoSuggestBox_SuggestionChosen(AutoSuggestBox sender, AutoSuggestBoxSuggestionChosenEventArgs args)
+        {
+            Contact temp = (Contact)args.SelectedItem;
+            if (temp == null)
+            {
+                NowChoose.Text = "";
+            }
+            NowChoose.Text = String.Format("{0} , {1}", temp.FullName,temp.Company);
+        }
+    }
+}
---
+++ 3/Model/Contact.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3.Model
+{
+    public class Contact
+    {
+        public Contact(string FirstName,string LastName,string Company)
+        {
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+            this.Company = Company;
+        }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName
+        {
+            get
+            {
+                return String.Format("{0} {1}", FirstName, LastName);
+            }
+        }
+        public string Company { get; set; }
+        public override string ToString()
+        {
+            return String.Format("{0} ---{1}", FullName, Company);
+        }
+    }
+}
