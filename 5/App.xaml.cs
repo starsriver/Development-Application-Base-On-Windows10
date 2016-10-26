6740388e3eb04692f2243ccb3a442f77305ae17b
starsriver <starsriver@outlook.com>
2016-10-26 19:44:30 +0800

    添加项目5：应用生命周期

---
+++ 5/App.xaml.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace _5
+{
+    /// <summary>
+    /// 提供特定于应用程序的行为，以补充默认的应用程序类。
+    /// </summary>
+    sealed partial class App : Application
+    {
+        /// <summary>
+        /// 初始化单一实例应用程序对象。这是执行的创作代码的第一行，
+        /// 已执行，逻辑上等同于 main() 或 WinMain()。
+        /// </summary>
+        public App()
+        {
+            this.InitializeComponent();
+            this.Suspending += OnSuspending;
+        }
+
+        /// <summary>
+        /// 在应用程序由最终用户正常启动时进行调用。
+        /// 将在启动应用程序以打开特定文件等情况下使用。
+        /// </summary>
+        /// <param name="e">有关启动请求和过程的详细信息。</param>
+        protected override void OnLaunched(LaunchActivatedEventArgs e)
+        {
+#if DEBUG
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                this.DebugSettings.EnableFrameRateCounter = true;
+            }
+#endif
+            Frame rootFrame = Window.Current.Content as Frame;
+
+            // 不要在窗口已包含内容时重复应用程序初始化，
+            // 只需确保窗口处于活动状态
+            if (rootFrame == null)
+            {
+                // 创建要充当导航上下文的框架，并导航到第一页
+                rootFrame = new Frame();
+
+                // 注册全局导航失败侦听器
+                rootFrame.NavigationFailed += OnNavigationFailed;
+
+                // 注册全局导航后退事件侦听器
+                Windows.UI.Core.SystemNavigationManager.GetForCurrentView().BackRequested += App_BackRequested;
+
+                if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
+                {
+                    //TODO: 从之前挂起的应用程序加载状态
+                }
+
+                // 将框架放在当前窗口中
+                Window.Current.Content = rootFrame;
+            }
+
+            //将激活时间和激活类型增量的储存到本地储存中
+            Windows.Storage.ApplicationDataContainer localTestData = Windows.Storage.ApplicationData.Current.LocalSettings;
+            string preveiousTestData = "";
+            if (localTestData.Values["test"]!=null)
+            {
+                preveiousTestData = (string)localTestData.Values["test"];
+            }
+            switch (e.PreviousExecutionState)
+            {
+                case ApplicationExecutionState.ClosedByUser:
+                    {
+                        preveiousTestData += "上次由用户关闭应用\n";
+                    }
+                    break;
+                case ApplicationExecutionState.NotRunning:
+                    {
+                        preveiousTestData += "应用初次运行\n";
+                    }
+                    break;
+                case ApplicationExecutionState.Running:
+                    {
+                        preveiousTestData += "上次应用正在运行\n";
+                    }
+                    break;
+                case ApplicationExecutionState.Suspended:
+                    {
+                        preveiousTestData += "上次应用被挂起\n";
+                    }
+                    break;
+                case ApplicationExecutionState.Terminated:
+                    {
+                        preveiousTestData += "上次应用挂起后被关闭\n";
+                    }
+                    break;
+            }
+            preveiousTestData += "应用已于" + DateTime.Now.ToString() + "激活\n";
+            localTestData.Values["test"] = preveiousTestData;
+
+
+            if (e.PrelaunchActivated == false)
+            {
+                if (rootFrame.Content == null)
+                {
+                    // 当导航堆栈尚未还原时，导航到第一页，
+                    // 并通过将所需信息作为导航参数传入来配置
+                    // 参数
+                    rootFrame.Navigate(typeof(MainPage), e.Arguments);
+                }
+                // 确保当前窗口处于活动状态
+                Window.Current.Activate();
+            }
+        }
+
+        /// <summary>
+        /// 导航到特定页失败时调用
+        /// </summary>
+        ///<param name="sender">导航失败的框架</param>
+        ///<param name="e">有关导航失败的详细信息</param>
+        void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
+        {
+            throw new Exception("Failed to load Page " + e.SourcePageType.FullName);
+        }
+
+        /// <summary>
+        /// 在将要挂起应用程序执行时调用。  在不知道应用程序
+        /// 无需知道应用程序会被终止还是会恢复，
+        /// 并让内存内容保持不变。
+        /// </summary>
+        /// <param name="sender">挂起的请求的源。</param>
+        /// <param name="e">有关挂起请求的详细信息。</param>
+        private void OnSuspending(object sender, SuspendingEventArgs e)
+        {
+            //将挂起时间和挂起提示增量的储存到本地储存中
+            Windows.Storage.ApplicationDataContainer localTestData = Windows.Storage.ApplicationData.Current.LocalSettings;
+            string testData = "";
+            if(localTestData.Values["test"]!=null)
+            {
+                testData = (string)localTestData.Values["test"];
+            }
+            testData += "应用已于" + DateTime.Now.ToString() + "挂起\n";
+            localTestData.Values["test"] = testData;
+
+            var deferral = e.SuspendingOperation.GetDeferral();
+            //TODO: 保存应用程序状态并停止任何后台活动
+            deferral.Complete();
+        }
+
+        /// <summary>
+        /// 侦听到系统导航后退事件时调用
+        /// 如果你想要从后退导航排除特定页面
+        /// 或想要在显示页面前执行页面级别代码
+        /// 可以在每个页面中注册此事件。
+        /// </summary>
+        /// <param name="sender">系统导航后退的框架</param>
+        /// <param name="e">有关系统导航后退的详细信息</param>
+        private void App_BackRequested(object sender, Windows.UI.Core.BackRequestedEventArgs e)
+        {
+            // 获取当前页面
+            Frame rootFrame = Window.Current.Content as Frame;
+            if (rootFrame == null)
+            {
+                return;
+            }
+            // 检测当前页面后退导航是否可用
+            if (rootFrame.CanGoBack && e.Handled == false)
+            {
+                e.Handled = true;
+                // 导航到上一页
+                rootFrame.GoBack();
+            }
+        }
+    }
+}
---
+++ 5/View/HomePage.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// “空白页”项模板在 http://go.microsoft.com/fwlink/?LinkId=234238 上有介绍
+
+namespace _5.View
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class HomePage : Page
+    {
+        public HomePage()
+        {
+            this.InitializeComponent();
+            //注册应用恢复事件
+            Application.Current.Resuming += new EventHandler<Object>(HomePage_Resuming);
+            rootFrame = Window.Current.Content as Frame;
+        }
+        private Frame rootFrame { get; set; }
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            //导航到此页面时，显示指定的本地事件
+            Windows.Storage.ApplicationDataContainer localTestData = Windows.Storage.ApplicationData.Current.LocalSettings;
+            if (localTestData.Values != null)
+            {
+                testTextBlock.Text = (string)localTestData.Values["test"];
+            }
+            // 设置显示导航栏后退按钮
+            if (rootFrame.CanGoBack)
+            {
+                SystemNavigationManager.GetForCurrentView().AppViewBackButtonVisibility = AppViewBackButtonVisibility.Visible;
+            }
+            else
+            {
+                SystemNavigationManager.GetForCurrentView().AppViewBackButtonVisibility = AppViewBackButtonVisibility.Collapsed;
+            }
+        }
+        /// <summary>
+        /// 应用恢复时调用
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void HomePage_Resuming(Object sender, Object e)
+        {
+            Windows.Storage.ApplicationDataContainer localTestData = Windows.Storage.ApplicationData.Current.LocalSettings;
+            string testData = "";
+            if (localTestData.Values["test"] != null)
+            {
+                testData = (string)localTestData.Values["test"];
+            }
+            testData += "应用于" + DateTime.Now.ToString() + "挂起后恢复\n";
+            localTestData.Values["test"] = testData;
+            testTextBlock.Text = (string)localTestData.Values["test"];
+        }
+    }
+}
