df5d2b6b8bc3ce028c6832b63239f3b4ab9eea2f
starsriver <starsriver@outlook.com>
2016-10-26 16:23:20 +0800

    更新NuGet包

--- 3/App.xaml.cs
+++ 3/App.xaml.cs
@@ -61,8 +61,6 @@ namespace _3
                 // 注册全局导航后退事件侦听器
                 Windows.UI.Core.SystemNavigationManager.GetForCurrentView().BackRequested += App_BackRequested;
 
-                
-
                 if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
                 {
                     //TODO: 从之前挂起的应用程序加载状态
@@ -134,53 +132,6 @@ namespace _3
                 // 导航到上一页
                 rootFrame.GoBack();
             }
-        }
-        protected override void OnSearchActivated(SearchActivatedEventArgs args)
-        {
-            Frame rootFrame = Window.Current.Content as Frame;
-
-            // 不要在窗口已包含内容时重复应用程序初始化，
-            // 只需确保窗口处于活动状态
-            if (rootFrame == null)
-            {
-                // 创建要充当导航上下文的框架，并导航到第一页
-                rootFrame = new Frame();
-
-                // 注册全局导航失败侦听器
-                rootFrame.NavigationFailed += OnNavigationFailed;
-
-                // 注册全局导航后退事件侦听器
-                Windows.UI.Core.SystemNavigationManager.GetForCurrentView().BackRequested += App_BackRequested;
-
-                // 将框架放在当前窗口中
-                Window.Current.Content = rootFrame;
-            }
-                if (rootFrame.Content == null)
-                {
-                    // 当导航堆栈尚未还原时，导航到第一页，
-                    // 并通过将所需信息作为导航参数传入来配置
-                    // 参数
-                    rootFrame.Navigate(typeof(MainPage), "OnSearch");
-                }
-                // 确保当前窗口处于活动状态
-                Window.Current.Activate();
-        }
-        public async static Task<string> AddAppContentFilesToIndexedFolder()
-        {
-            var localFolder = Windows.Storage.ApplicationData.Current.LocalFolder;
-            var installDirectory = Windows.ApplicationModel.Package.Current.InstalledLocation;
-            var outputString = "Items added to the \"Indexed\" folder:";
-            var appContentFolder = await installDirectory.GetFolderAsync("SearchIndexFile");
-            var indexedFolder = await localFolder.CreateFolderAsync(
-                "Indexed", Windows.Storage.CreationCollisionOption.OpenIfExists);
-            var files = await appContentFolder.GetFilesAsync();
-            foreach (var file in files)
-            {
-                outputString += "\n" + file.DisplayName + file.FileType;
-                await file.CopyAsync(indexedFolder,
-                    file.Name, Windows.Storage.NameCollisionOption.ReplaceExisting);
-            }
-            return outputString;
         }
     }
 }
